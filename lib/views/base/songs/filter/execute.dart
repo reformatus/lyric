e2abc1510cd1ffa076de0fc4a82a8db324ea57d5
Fodor Benedek <benedek@fodor.pro>
2024-10-05 11:49:08 +0200

    execute logic for existent fields in song bank

---
+++ lib/views/base/songs/filter/execute.dart
@@ -0,0 +1,65 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../../data/bank/bank.dart';
+import '../../../../data/song/song.dart';
+import 'state.dart';
+
+part 'execute.g.dart';
+
+// todo make provider depend on all songs
+// todo make part of bank
+// todo write test
+Map<String, ({FieldType type, int count})> existingSearchableFields() {
+  Map<String, ({FieldType type, int count})> fields = {};
+
+  for (var song in allSongs) {
+    for (var field in song.content.keys) {
+      if (FieldType.fromString(songFieldsMap[field]?['type'] ?? "")?.isSearchable ?? false) {
+        if (!fields.keys.any((k) => k == field)) {
+          // first time we see this field
+          fields[field] = (type: FieldType.fromString(songFieldsMap[field]!['type'])!, count: 1);
+        } else {
+          // increment count by reassigning the record for the entry
+          fields[field] = (type: fields[field]!.type, count: fields[field]!.count + 1);
+        }
+      }
+    }
+  }
+
+  return fields;
+}
+
+// todo make provider depend on all songs
+// todo make part of bank
+// todo write test
+Map<String, ({FieldType type, int count})> existingFilterableFields() {
+  Map<String, ({FieldType type, int count})> fields = {};
+
+  for (var song in allSongs) {
+    for (var field in song.content.keys) {
+      if (FieldType.fromString(songFieldsMap[field]?['type'] ?? "")?.isFilterable ?? false) {
+        if (!fields.keys.any((k) => k == field)) {
+          // first time we see this field
+          fields[field] = (type: FieldType.fromString(songFieldsMap[field]!['type'])!, count: 1);
+        } else {
+          // increment count by reassigning the record for the entry
+          fields[field] = (type: fields[field]!.type, count: fields[field]!.count + 1);
+        }
+      }
+    }
+  }
+
+  return fields;
+}
+
+@riverpod
+Future<List<Song>> filteredSongList(FilteredSongListRef ref) async {
+  final state = ref.watch(filterStateProvider);
+
+  // todo actually apply filters
+
+  //await Future.delayed(const Duration(seconds: 1));
+  //throw UnimplementedError();
+
+  return allSongs.toList();
+}
