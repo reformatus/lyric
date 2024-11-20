3b32e708f5a5507c1fd322fcbf888d5b674189f5
Fodor Benedek <benedek@fodor.pro>
2024-11-20 14:29:54 +0100

    add todo

--- lib/services/song/from_uuid.dart
+++ lib/services/song/from_uuid.dart
@@ -7,6 +7,8 @@ import '../../data/song/song.dart';
 
 part 'from_uuid.g.dart';
 
+
+// todo make simple wrappers like this for all db access to make migration from drift possible if necessary
 @riverpod
 Future<Song> songFromUuid(Ref ref, String uuid) async {
   return (await (db.songs.select()..where((s) => s.uuid.equals(uuid))).getSingle());
