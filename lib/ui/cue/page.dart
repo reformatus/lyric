199fde3528dfe43feef4bcb2b3e831b374612828
Fodor Benedek <benedek@fodor.pro>
2024-11-23 12:36:46 +0100

    cue page

---
+++ lib/ui/cue/page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lyric/data/cue/slide.dart';
+import 'package:lyric/services/cue/from_id.dart';
+import 'package:lyric/ui/common/error.dart';
+
+class CuePage extends ConsumerStatefulWidget {
+  const CuePage(this.cueId, {super.key});
+
+  final int cueId;
+
+  @override
+  ConsumerState<CuePage> createState() => _CuePageState();
+}
+
+class _CuePageState extends ConsumerState<CuePage> {
+  @override
+  Widget build(BuildContext context) {
+    final cue = ref.watch(revivedCueFromIdProvider(widget.cueId));
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(cue.value?.title ?? ''),
+        ),
+        body: switch (cue) {
+          AsyncError(:final error, :final stackTrace) => LErrorCard(
+              type: LErrorType.error,
+              title: 'Nem sikerült betölteni a listát!',
+              icon: Icons.error,
+              message: error.toString(),
+              stack: stackTrace.toString(),
+            ),
+          AsyncLoading() => Center(
+              child: CircularProgressIndicator(),
+            ),
+          AsyncValue(:final value!) => ListView(
+              children: value.slides!.map((e) => CueSlideTile(e)).toList(),
+            )
+        });
+  }
+}
+
+class CueSlideTile extends StatelessWidget {
+  const CueSlideTile(this.slide, {super.key});
+
+  final Slide slide;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(slide.getPreview()),
+    );
+  }
+}
