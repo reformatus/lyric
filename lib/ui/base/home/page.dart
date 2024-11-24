e6578edcac48a720589af8a96b1adf593a76620a
Fodor Benedek <benedek@fodor.pro>
2024-11-24 01:19:36 +0100

    app name

--- lib/ui/base/home/page.dart
+++ lib/ui/base/home/page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sófár Lyric'),
+        title: const Text('Sófár DalApp'),
       ),
       body: Column(
         children: [
