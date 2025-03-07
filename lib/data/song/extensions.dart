b8b64afcb025eb61987144abe9279534cbb8b409
Fodor Benedek <benedek@fodor.pro>
2025-03-07 13:53:18 +0100

    add song property getters

---
+++ lib/data/song/extensions.dart
@@ -0,0 +1,14 @@
+part of 'song.dart';
+
+extension PropertyUtils on Song {
+  bool get hasSvg => contentMap['svg']?.isNotEmpty ?? false;
+
+  bool get hasPdf => contentMap['pdf']?.isNotEmpty ?? false;
+  
+  // TODO figure out api for non-opensong chord notation
+  bool get hasLyrics => contentMap['opensong']?.isNotEmpty ?? false;
+  
+  // TODO figure out api for non-opensong chord notation
+  /// Matches for a `.` after a new line (for OpenSong chord formatting)
+  bool get hasChords => hasLyrics ? RegExp(r'\n\.').hasMatch(contentMap['opensong']?? "") : false;
+}
