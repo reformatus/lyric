abcc23e76357f802ba8cbeb5cc64ecf4ee97916d
Fodor Benedek <benedek@fodor.pro>
2025-03-25 17:07:18 +0100

    fix warning

--- lib/ui/song/add_to_cue_search.dart
+++ lib/ui/song/add_to_cue_search.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:lyric/data/cue/cue.dart';
 import 'package:lyric/data/song/song.dart';
 import 'package:lyric/data/song/transpose.dart';
