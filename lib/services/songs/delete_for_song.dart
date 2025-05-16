c3599af307d1c883e1f15a6370077e0163c0dd32
Fodor Benedek <benedek@fodor.pro>
2025-05-16 12:00:10 +0200

    add doc

--- lib/services/songs/delete_for_song.dart
+++ lib/services/songs/delete_for_song.dart
@@ -3,6 +3,7 @@ import 'package:drift/drift.dart';
 import '../../data/database.dart';
 import '../../data/song/song.dart';
 
+/// Deletes all assets associated with a given song.
 Future<int> deleteAssetsForSong(Song song) async {
   return await db.assets.deleteWhere((a) => a.songUuid.equals(song.uuid));
 }
