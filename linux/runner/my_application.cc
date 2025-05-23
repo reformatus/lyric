ba608473adbf2c87879d61fb2d7a965d3b0d2330
Fodor Benedek <benedek@fodor.pro>
2025-05-23 10:20:29 +0200

    deep linking setup on Windows, macOS, Linux

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -17,6 +17,15 @@ G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
 // Implements GApplication::activate.
 static void my_application_activate(GApplication* application) {
   MyApplication* self = MY_APPLICATION(application);
+
+  //! DEEP LINKING
+  GList* windows = gtk_application_get_windows(GTK_APPLICATION(application));
+  if (windows) {
+    gtk_window_present(GTK_WINDOW(windows->data));
+    return;
+  }
+  // /DEEP LINKING
+
   GtkWindow* window =
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
 
@@ -78,7 +87,8 @@ static gboolean my_application_local_command_line(GApplication* application, gch
   g_application_activate(application);
   *exit_status = 0;
 
-  return TRUE;
+  //! MODIFIED FOR DEEP LINKING @see https://github.com/llfbandit/app_links/blob/master/doc/README_linux.md
+  return FALSE;
 }
 
 // Implements GApplication::startup.
@@ -123,8 +133,9 @@ MyApplication* my_application_new() {
   // the application to be recognized beyond its binary name.
   g_set_prgname(APPLICATION_ID);
 
+  //! MODIFIED FOR DEEP LINKING @see https://github.com/llfbandit/app_links/blob/master/doc/README_linux.md
   return MY_APPLICATION(g_object_new(my_application_get_type(),
                                      "application-id", APPLICATION_ID,
-                                     "flags", G_APPLICATION_NON_UNIQUE,
+                                     "flags", G_APPLICATION_HANDLES_COMMAND_LINE | G_APPLICATION_HANDLES_OPEN,
                                      nullptr));
 }
--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,6 +1,23 @@
 import Cocoa
 import FlutterMacOS
 
+//! DEEP LINKING
+import app_links
+
+public override func application(_ application: NSApplication,
+                                  continue userActivity: NSUserActivity,
+                                  restorationHandler: @escaping ([any  NSUserActivityRestoring]) -> Void) -> Bool {
+
+  guard let url = AppLinks.shared.getUniversalLink(userActivity) else {
+    return false
+  }
+  
+  AppLinks.shared.handleLink(link: url.absoluteString)
+  
+  return false // Returning true will stop the propagation to other packages
+}
+// /DEEP LINKING
+
 @main
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
