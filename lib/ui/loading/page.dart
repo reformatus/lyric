69c72a7e0f9ce08f68e4f6a14bb4d60425f97634
Fodor Benedek <benedek@fodor.pro>
2025-05-24 17:47:19 +0200

    rm unused imports

--- lib/ui/loading/page.dart
+++ lib/ui/loading/page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:lyric/data/log/logger.dart';
 
 import '../../main.dart';
 import '../../services/bank/bank_updated.dart';
