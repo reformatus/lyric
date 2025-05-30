550c884d5538e2068948a6ce3d610570f7e58015
Fodor Benedek <benedek@fodor.pro>
2025-05-30 14:33:24 +0200

    write bank services init

---
+++ lib/services/bank/write_bank.dart
@@ -0,0 +1,9 @@
+import '../../data/bank/bank.dart';
+
+Future setBankEnabled(bool enabled, Bank bank) {
+  throw UnimplementedError();
+}
+
+Future setBankOfflineMode(bool offlineMode, Bank bank) {
+  throw UnimplementedError();
+}
