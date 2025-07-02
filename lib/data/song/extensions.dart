33b1f5973aefef83522a1885e3be2f53472c7537
Fodor Benedek <benedek@fodor.pro>
2025-07-02 18:00:53 +0200

    formatting

--- lib/data/song/extensions.dart
+++ lib/data/song/extensions.dart
@@ -4,11 +4,13 @@ extension PropertyUtils on Song {
   bool get hasSvg => contentMap['svg']?.isNotEmpty ?? false;
 
   bool get hasPdf => contentMap['pdf']?.isNotEmpty ?? false;
-  
+
   // TODO figure out api for non-opensong chord notation
   bool get hasLyrics => contentMap['opensong']?.isNotEmpty ?? false;
-  
+
   // TODO figure out api for non-opensong chord notation
   /// Matches for a `.` after a new line (for OpenSong chord formatting)
-  bool get hasChords => hasLyrics ? RegExp(r'\n\.').hasMatch(contentMap['opensong']?? "") : false;
+  bool get hasChords => hasLyrics
+      ? RegExp(r'\n\.').hasMatch(contentMap['opensong'] ?? "")
+      : false;
 }
