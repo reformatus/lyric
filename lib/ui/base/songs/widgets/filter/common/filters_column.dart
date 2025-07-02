fa76a9c407109aa640ed152797193e299d8790e7
Fodor Benedek <benedek@fodor.pro>
2025-07-02 18:02:05 +0200

    order filters based on count of songs

--- lib/ui/base/songs/widgets/filter/common/filters_column.dart
+++ lib/ui/base/songs/widgets/filter/common/filters_column.dart
@@ -14,42 +14,45 @@ class FiltersColumn extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     var filterableFieldsList = ref.watch(existingFilterableFieldsProvider);
 
-    return switch (filterableFieldsList) {
-      AsyncError(:final error, :final stackTrace) => LErrorCard(
-        type: LErrorType.error,
-        title: 'Hiba a szűrők betöltése közben',
-        message: error.toString(),
-        stack: stackTrace.toString(),
-        icon: Icons.error,
-      ),
-      AsyncValue(:final value) =>
-        value == null
-            ? Center(child: LinearProgressIndicator())
-            : Column(
-              mainAxisSize: MainAxisSize.min,
-              children:
-                  value.entries
-                      .map(
-                        (e) => switch (e.value.type) {
-                          FieldType.multiselect ||
-                          FieldType.multiselectTags => MultiselectFilterCard(
-                            field: e.key,
-                            fieldType: e.value.type,
-                            fieldPopulatedCount: e.value.count,
-                          ),
-                          FieldType.key => KeyFilterCard(
-                            fieldPopulatedCount: e.value.count,
-                          ),
-                          _ => LErrorCard(
-                            type: LErrorType.warning,
-                            title: 'Nem támogatott szűrőtípus!',
-                            message: e.value.toString(),
-                            icon: Icons.filter_alt,
-                          ),
-                        },
-                      )
-                      .toList(),
-            ),
-    };
+    switch (filterableFieldsList) {
+      case AsyncError(:final error, :final stackTrace):
+        return LErrorCard(
+          type: LErrorType.error,
+          title: 'Hiba a szűrők betöltése közben',
+          message: error.toString(),
+          stack: stackTrace.toString(),
+          icon: Icons.error,
+        );
+      case AsyncValue(:final value):
+        if (value == null) return Center(child: LinearProgressIndicator());
+
+        var filterList = value.entries.toList();
+        filterList.sort((a, b) => a.value.count.compareTo(b.value.count));
+
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: filterList.reversed
+              .map(
+                (e) => switch (e.value.type) {
+                  FieldType.multiselect ||
+                  FieldType.multiselectTags => MultiselectFilterCard(
+                    field: e.key,
+                    fieldType: e.value.type,
+                    fieldPopulatedCount: e.value.count,
+                  ),
+                  FieldType.key => KeyFilterCard(
+                    fieldPopulatedCount: e.value.count,
+                  ),
+                  _ => LErrorCard(
+                    type: LErrorType.warning,
+                    title: 'Nem támogatott szűrőtípus!',
+                    message: e.value.toString(),
+                    icon: Icons.filter_alt,
+                  ),
+                },
+              )
+              .toList(),
+        );
+    }
   }
 }
