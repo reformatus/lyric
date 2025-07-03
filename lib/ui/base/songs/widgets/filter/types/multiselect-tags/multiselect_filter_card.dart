4c250fc34f87e2c7ec732c0e59db205f0fa5523f
Fodor Benedek <benedek@fodor.pro>
2025-07-03 15:59:29 +0200

    factor out base filter card (thanks claude)

--- lib/ui/base/songs/widgets/filter/types/multiselect-tags/multiselect_filter_card.dart
+++ lib/ui/base/songs/widgets/filter/types/multiselect-tags/multiselect_filter_card.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../../../../services/songs/filter.dart';
 import '../../../../../../common/error/card.dart';
+import '../../common/base_filter_card.dart';
 import '../../common/filter_chip.dart';
 import '../field_type.dart';
 import 'state.dart';
@@ -48,56 +49,28 @@ class LFilterChipsState extends ConsumerState<MultiselectFilterCard> {
 
     final isActive = filterState.containsKey(widget.field);
 
-    return Card(
-      elevation: isActive ? 3 : 0,
-      color: isActive ? Theme.of(context).colorScheme.secondaryContainer : null,
-      child: ListTile(
-        contentPadding: const EdgeInsets.symmetric(horizontal: 8),
-        leading:
-            !isActive
-                ? Padding(
-                  padding: const EdgeInsets.only(left: 8),
-                  child: Icon(
-                    songFieldsMap[widget.field]!['icon'] ?? Icons.filter_list,
-                  ),
-                )
-                : SizedBox(
-                  width: 32,
-                  child: IconButton(
-                    visualDensity: VisualDensity.compact,
-                    onPressed:
-                        () =>
-                            filterStateNotifier.resetFilterField(widget.field),
-                    icon: Icon(Icons.clear),
-                  ),
-                ),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              // far future todo: i18n
-              songFieldsMap[widget.field]!['title_hu'] ??
-                  "[Szűrő neve hiányzik]",
-            ),
-            Expanded(
-              child: Text(
-                "  ${widget.fieldPopulatedCount} kitöltve",
-                maxLines: 1,
-                softWrap: false,
-                textAlign: TextAlign.end,
-                overflow: TextOverflow.fade,
-                style: TextStyle(
-                  fontStyle: FontStyle.italic,
-                  color: Theme.of(context).colorScheme.onSecondaryContainer,
-                  fontSize: Theme.of(context).textTheme.bodySmall!.fontSize,
-                ),
-              ),
-            ),
-          ],
+    return BaseFilterCard(
+      icon: songFieldsMap[widget.field]!['icon'] ?? Icons.filter_list,
+      title:
+          songFieldsMap[widget.field]!['title_hu'] ?? "[Szűrő neve hiányzik]",
+      isActive: isActive,
+      onResetPressed: isActive
+          ? () => filterStateNotifier.resetFilterField(widget.field)
+          : null,
+      trailing: Text(
+        "  ${widget.fieldPopulatedCount} kitöltve",
+        maxLines: 1,
+        softWrap: false,
+        textAlign: TextAlign.end,
+        overflow: TextOverflow.fade,
+        style: TextStyle(
+          fontStyle: FontStyle.italic,
+          color: Theme.of(context).colorScheme.onSecondaryContainer,
+          fontSize: Theme.of(context).textTheme.bodySmall!.fontSize,
         ),
-        subtitle: switch (selectableValues) {
+      ),
+      children: [
+        switch (selectableValues) {
           AsyncError(:final error, :final stackTrace) => LErrorCard(
             title: 'Hiba a szűrőértékek lekérdezése közben',
             icon: Icons.warning,
@@ -107,42 +80,43 @@ class LFilterChipsState extends ConsumerState<MultiselectFilterCard> {
           ),
           AsyncValue(:final value) =>
             value == null
-                ? LinearProgressIndicator()
+                ? const LinearProgressIndicator()
                 : SizedBox(
-                  height: 42,
-                  child: FadingEdgeScrollView.fromScrollView(
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      controller: _filterChipsRowController,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: value.length,
-                      itemBuilder: (context, i) {
-                        // todo move all logic to service (like in filter/key/widget.dart)
-                        String item = value[i];
-                        bool selected =
-                            filterState[widget.field]?.contains(item) ?? false;
-                        onSelected(bool newValue) {
-                          if (newValue) {
-                            filterStateNotifier.addFilter(widget.field, item);
-                          } else {
-                            filterStateNotifier.removeFilter(
-                              widget.field,
-                              item,
-                            );
+                    height: 42,
+                    child: FadingEdgeScrollView.fromScrollView(
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        controller: _filterChipsRowController,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: value.length,
+                        itemBuilder: (context, i) {
+                          // todo move all logic to service (like in filter/key/widget.dart)
+                          String item = value[i];
+                          bool selected =
+                              filterState[widget.field]?.contains(item) ??
+                              false;
+                          onSelected(bool newValue) {
+                            if (newValue) {
+                              filterStateNotifier.addFilter(widget.field, item);
+                            } else {
+                              filterStateNotifier.removeFilter(
+                                widget.field,
+                                item,
+                              );
+                            }
                           }
-                        }
 
-                        return LFilterChip(
-                          label: item,
-                          onSelected: onSelected,
-                          selected: selected,
-                        );
-                      },
+                          return LFilterChip(
+                            label: item,
+                            onSelected: onSelected,
+                            selected: selected,
+                          );
+                        },
+                      ),
                     ),
                   ),
-                ),
         },
-      ),
+      ],
     );
   }
 }
